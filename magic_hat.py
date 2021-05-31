5f3b3b53e67de333910735ddc76ebccbd5cc85fc
Ly Nguyen <lynguyen237@Lys-MacBook-Pro.local>
2021-05-30 21:56:41 -1000

    Add ask_at_interval() function to ask the Magic Hat to give a question at x-second interval using Timer method from threading module

--- magic_hat.py
+++ magic_hat.py
@@ -6,25 +6,50 @@ Ask the hat to turn off the periodic questions
 The hat should not repeat questions that have been asked before (unless they've all been asked already)
 """
 
-import random
+import random, threading
+import time
 
 
 questions = ["What’s the best thing you’ve got going on in your life at the moment?",
              "What incredibly common thing have you never done?",
              "What has taken you the longest to get good or decent at?"]
 
+
 class MagicHat:
 
-    def __init__(self, questionbank):
-        self.questionbank = questionbank
+    def __init__(self, question_bank):
+        self.question_bank = question_bank
+        self.thread = None # Start a thread variable to activate periodic questions later
         self.asked_questions = None
 
-    def give_question(self):
+    def ask(self):
         """ Ask the hat to give a random question """
-        print(random.choice(self.questionbank))
+        print("Here is your question from the Magic Hat:")
+        print(random.choice(self.unasked_questions), "\n")
+
+    
+    def ask_at_interval(self, seconds=30):
+        """ 
+        Ask the hat to give a question every x seconds using
+        Python Timer Objects https://docs.python.org/3/library/threading.html#timer-objects.
+        The default interval if no argument is provided is 30 seconds
+        """   
+        self.ask()
+        self.thread = threading.Timer(seconds, self.ask_at_interval, [seconds])
+        self.thread.start() #Start the thread running periodic questions
+        
+    def stop(self):
+        """ Ask the hat to stop periodic questions initiated by function ask_at_interval """
+        if not self.thread:
+            print("You have not asked the Magic Hat to ask periodic questions yet so there is nothing to stop.")
+        else:
+            self.thread.cancel() #Stop the thread running periodic questions
+
+mh = MagicHat(questions)
 
 
-magic_hat = MagicHat(questions)
+
+
 
 
 
